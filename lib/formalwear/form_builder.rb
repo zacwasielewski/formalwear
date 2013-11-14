f4c51ab0b32eb7ab1f4e64b23480c2351cb224a7
Zac Wasielewski <zac@wasielewski.org>
2013-11-14 17:14:47 -0500

    Add support for grids and form groups

--- lib/formalwear/form_builder.rb
+++ lib/formalwear/form_builder.rb
@@ -5,7 +5,7 @@ class Formalwear::FormBuilder < ActionView::Helpers::FormBuilder
 
   def group(options = nil)
     options ||= {}
-    classes = merge_wordlists( options[:class], 'form-group')
+    classes = merge_classes( options[:class], 'form-group')
     options.merge!( :class => classes )
     @template.content_tag(:div, nil, options) do
       if block_given?
@@ -14,8 +14,9 @@ class Formalwear::FormBuilder < ActionView::Helpers::FormBuilder
     end
   end
 
-  def label(method, text = nil, options = {}, &block)
-    classes = merge_wordlists( options[:class], 'control-label')
+  def label(method, text = nil, options = nil, &block)
+    options ||= {}
+    classes = merge_classes( options[:class], 'control-label')
     options.merge! :class => classes
     super
   end
@@ -29,34 +30,118 @@ class Formalwear::FormBuilder < ActionView::Helpers::FormBuilder
     end
   end
   
-  def text_field(name, options = nil)
+  def text_field(method, options = nil)
     options ||= {}
-    classes = merge_wordlists( options[:class], 'form-control')
+    classes = merge_classes( options[:class], 'form-control')
     options.reverse_merge! :class => classes
     super
   end
 
-  def email_field(name, options = nil)
+  def email_field(method, options = nil)
     options ||= {}
-    classes = merge_wordlists( options[:class], 'form-control')
+    classes = merge_classes( options[:class], 'form-control')
     options.reverse_merge! :class => classes
     super
   end
   
-  def password_field(name, options = nil)
+  def password_field(method, options = nil)
     options ||= {}
-    classes = merge_wordlists( options[:class], 'form-control')
+    classes = merge_classes( options[:class], 'form-control')
     options.reverse_merge! :class => classes
     super
+  end
+
+  def text_field_group(method, text = nil, options = nil, label_options = nil, control_options = nil)
+    options         ||= {}
+    label_options   ||= {}
+    control_options ||= {}
+    if label_grid = label_options.delete(:grid)
+      classes = merge_classes( label_options[:class], grid_classes(label_grid))
+      label_options[:class] = classes
+    end
+    if control_grid = control_options.delete(:grid)
+      classes = merge_classes( control_options[:class], grid_classes(control_grid))
+      control_wrapper_class = classes
+    end
+    group(options) do
+      html = ""
+      html += label(method, text, label_options)
+      if control_grid
+        html += @template.content_tag(:div, nil, { :class => control_wrapper_class }) { text_field(method, control_options) }
+      else
+        html += text_field(method, control_options)
+      end
+      html.html_safe
+    end
+  end
+
+  def email_field_group(method, text = nil, options = nil, label_options = nil, control_options = nil)
+    options         ||= {}
+    label_options   ||= {}
+    control_options ||= {}
+    if label_grid = label_options.delete(:grid)
+      classes = merge_classes( label_options[:class], grid_classes(label_grid))
+      label_options[:class] = classes
+    end
+    if control_grid = control_options.delete(:grid)
+      classes = merge_classes( control_options[:class], grid_classes(control_grid))
+      control_wrapper_class = classes
+    end
+    group(options) do
+      html = ""
+      html += label(method, text, label_options)
+      if control_grid
+        html += @template.content_tag(:div, nil, { :class => control_wrapper_class }) { email_field(method, control_options) }
+      else
+        html += email_field(method, control_options)
+      end
+      html.html_safe
+    end
+  end
+
+  def password_field_group(method, text = nil, options = nil, label_options = nil, control_options = nil)
+    options         ||= {}
+    label_options   ||= {}
+    control_options ||= {}
+    if label_grid = label_options.delete(:grid)
+      classes = merge_classes( label_options[:class], grid_classes(label_grid))
+      label_options[:class] = classes
+    end
+    if control_grid = control_options.delete(:grid)
+      classes = merge_classes( control_options[:class], grid_classes(control_grid))
+      control_wrapper_class = classes
+    end
+    group(options) do
+      html = ""
+      html += label(method, text, label_options)
+      if control_grid
+        html += @template.content_tag(:div, nil, { :class => control_wrapper_class }) { password_field(method, control_options) }
+      else
+        html += password_field(method, control_options)
+      end
+      html.html_safe
+    end
   end
 
 private
   
-  def merge_wordlists(list1 = nil, list2 = nil, delim = nil)
+  def merge_classes(a = nil, b = nil, delim = nil)
     delim ||= " "
-    list1 ||= ""
-    list2 ||= ""
-    [list1.split(delim), list2.split(delim)].flatten.uniq.join(delim)
+    a ||= ""
+    b ||= ""
+    [a.split(delim), b.split(delim)].flatten.uniq.join(delim)
+  end
+  
+  def grid_classes(grid = nil)
+    grid ||= {}
+    classes = []
+    classes << 'col-sm-'+grid[:sm].to_s if grid[:sm].is_a? Integer
+    classes << 'col-md-'+grid[:md].to_s if grid[:md].is_a? Integer
+    classes << 'col-lg-'+grid[:lg].to_s if grid[:lg].is_a? Integer
+    classes << 'col-offset-sm-'+grid[:offset_sm].to_s if grid[:offset_sm].is_a? Integer
+    classes << 'col-offset-md-'+grid[:offset_md].to_s if grid[:offset_md].is_a? Integer
+    classes << 'col-offset-lg-'+grid[:offset_lg].to_s if grid[:offset_lg].is_a? Integer
+    classes.uniq.join(" ")
   end
 
 end
